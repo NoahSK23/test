b1a449b48543741dbcb8c35c98defa573609855e
Noah <Noah@192.168.1.6>
2018-12-20 17:04:24 -0500

    idk what i'm doing lols

--- test/src/test.java
+++ test/src/test.java
@@ -2,6 +2,7 @@
 public class test {
 	public static void main(String[] args) {
 		System.out.println("This is a test. Oh yeah, and HELLO WORLD");
+		// So, I'm making a change and seeing how this works. Did it work????
 	}
 
 }
