a09549a04bdc5c47c66e905e396717e29a847f9c
Noah <Noah@192.168.1.6>
2018-12-21 01:28:37 -0500

    Added a sysout line about 3+3

--- test/src/test.java
+++ test/src/test.java
@@ -5,6 +5,7 @@ public class test {
 		// So, I'm making a change and seeing how this works. Did it work????
 		// And now, I am on my desktop. DID IT WORK? Probably it was working before lmao
 		// ha.
+		System.out.println("Hey, want to know what 3 + 3 is? it's: " + 6);
 	}
 
 }
