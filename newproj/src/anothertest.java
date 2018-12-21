fdd83ef2b74cd78c08e4d9b20c15f8cfef3877af
Noah <Noah@DESKTOP-R24N5E5.home>
2018-12-21 01:31:40 -0500

    First Commit - new project test

---
+++ newproj/src/anothertest.java
@@ -0,0 +1,8 @@
+
+public class anothertest {
+	public static void main(String[] args) {
+		System.out.println(
+				"Just another test.. again. This time, I made a new project. seeing how this works with git. Hope it works well!!");
+	}
+
+}
